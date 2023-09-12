2e282047683d871db75db4732acae72c7a52bdb2
214490195 <214490195@LAB41PC9>
2023-09-12 18:32:46 +0300

    hello

---
+++ Stage0/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0195();
+            Welcome8204();
+            Console.ReadKey();
+        }
+
+        private static void Welcome0195()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome8204();
+
+    }
+}
