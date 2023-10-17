b7a1cea20a545443c9cddfecf549f8a8240a0330
Debbie Heller <Debbie Heller@DEBBIE>
2023-10-17 18:41:26 +0300

    i add a class

---
+++ Stage0/Program8204.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome8204()
+        {
+           
+            Console.WriteLine("im so hungry and nervous ");
+           
+
+        }
+    }
+}
