f8830533fbfc09f47b8aedfe0b9cd45c234b1922
chaya.aliza195 <chaya.aliza195@gmail.com>
2023-10-17 18:25:13 +0300

    delete

--- Stage0/Program0195.cs
+++ Stage0/Program0195.cs
@@ -14,7 +14,6 @@
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
             Console.Write("{0}, welcome to my first console application", userName);
-            Console.Beep();
         }
         static partial void Welcome8204();
 
