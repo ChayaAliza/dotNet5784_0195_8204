d37656e72007f795d48ed07d6db2cff959fb3784
214490195 <214490195@LAB41PC9>
2023-12-26 15:12:37 +0200

    התחלת שלב 4

--- DalList/Dallist.cs
+++ DalList/Dallist.cs
@@ -1,9 +1,11 @@
 ﻿namespace Dal;
 using DalApi;
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
     public IDependency Dependency => new DependencyImplementation();
     public ITask Task => new TaskImplementation();
     public IEngineer Engineer => new EngineerImplementation();
-    
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
+
 };
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,13 +1,19 @@
 ﻿
 using DalApi;
+using System.ComponentModel;
+using System.Diagnostics;
+
 namespace Dal;
 
 //stage 3
-sealed public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
     public IDependency Dependency => new DependencyImplementation();
 
     public ITask Task => new TaskImplementation();
 
     public IEngineer Engineer => new EngineerImplementation();
+
+    public static IDal Instance { get; } = new DalXml();
+    public DalXml() { }
 }
